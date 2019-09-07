82139a5dc01e045d6ce28f7acc50780f6196fe18
Jas Spencer <jas.spencer414@gmail.com>
2019-09-07 18:34:25 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -47,7 +47,7 @@ def hit?(card_total)
 end
 
 def invalid_command
-  # code invalid_command here
+puts "PLease enter a valid command"  # code invalid_command here
 end
 
 #####################################################
